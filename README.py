6a9e056cf350136ff8521a72f16c77b198aca84e
srh-ai <sarah.aissa@ens.uvsq.fr>
2025-03-09 23:07:43 +0100

    Update README.py

--- README.py
+++ README.py
@@ -91,3 +91,21 @@ def jeu_pendu():
     print("bienvenue au jeu du pendu!")
     print(dessinPendu((0))
     
+while essais_restants >0
+    # affiche le mot avec les lettres devinées et les lettres manquantes en '_'
+    affichage_mot= ' '.joint([lettre if lettre in lettres-trouves else '_' for in mot_a_deviner])
+    print("mot a deviner :" + affichage_mot
+    print("Lettres essayées:" + ',' , join(lettres_essayees))
+
+    #demande une lettre a l'utilisateur 
+    lettre = input("Proposez une lettre:").upper()
+
+    #verifie que l'entrée est une lettre unique 
+    if len(lettre) != 1 or not lettre.isalpha():
+       print("Veuillez entrer une seule lettr.")
+       continue 
+
+    # verifie si la lettre a déja été proposée 
+    if lettre in lettres_essayees: 
+       print("Vous avez déja essayé cette lettre."
+    continue
